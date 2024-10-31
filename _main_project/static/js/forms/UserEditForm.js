1017cf6a6a7c7ac874b755149c65e656e60e2b75
Drew <91377105+Drewski6@users.noreply.github.com>
2024-10-31 18:03:12 +0100

    Getting UserEditForm ready. but nothing implemented yet.

--- _main_project/static/js/forms/UserEditForm.js
+++ _main_project/static/js/forms/UserEditForm.js
@@ -8,7 +8,8 @@ export default class extends AbstractModalView {
 
   async getHtml() {
     try {
-      const csrfToken = this.getCookie('csrftoken');
+      // const csrfToken = this.getCookie('csrftoken');
+      // <input type="hidden" name="csrfmiddlewaretoken" value="${csrfToken}">
       const loginResponse = await fetch('/login_check/');
       const loginData = await loginResponse.json();
       const userResponse = await fetch(`http://localhost:8000/user/${loginData.id}/`);
@@ -16,7 +17,6 @@ export default class extends AbstractModalView {
       return `
         <img src="${userData.profile_image}" alt="user image" style="width: 100px; height: 100px;"><br>
         <form id="editUserForm" onsubmit="event.preventDefault();">
-          <input type="hidden" name="csrfmiddlewaretoken" value="${csrfToken}">
           <input type="file" name="profile_image"><br>
           <h6>Username</h6>
           <input type="text" name="username" placeholder="Username" required value="${userData.username}"><br>
@@ -33,5 +33,54 @@ export default class extends AbstractModalView {
       console.log(error);
       return "";
     }
+  }
+
+  async afterRender() {
+
+    // This is all copied from RegisterForm and has not yet been modified to fit this form
+
+    // document.getElementById('editUserForm').addEventListener('submit', async(event) => {
+    //   // Create form 
+    //   const form = event.target;
+    //   const formData = new FormData(form);
+    //   const data = {};
+    //   formData.forEach((value, key) => {
+    //     data[key] = value;
+    //   });
+
+    //   const content = {
+    //     method: 'POST',
+    //     headers: {
+    //       'Accept': 'application/json',
+    //       'Content-Type': 'application/json',
+    //       'X-CSRFToken': this.getCookie('csrftoken')
+    //     },
+    //     body: JSON.stringify(data)
+    //   };
+
+    //   try {
+    //     const response = await fetch("/register/", content);
+    //     const result = await response.json();
+    //     const messageDiv = document.getElementById('message');
+
+    //     if (response.ok) {
+    //       messageDiv.textContent = result.message;
+    //       if (result.redirect) {
+    //         this.modal.hide();
+    //         navigateTo(result.redirect);
+    //       }
+    //       if (result.profile_image_url) {
+    //         const userPic = document.getElementById('userPic');
+    //         userPic.src = result.profile_image_url;
+    //       }
+    //     } else {
+    //       messageDiv.textContent = JSON.stringify(result.errors);
+    //     }
+    //   } catch (error) {
+    //     console.error('Error:', error);
+    //     const messageDiv = document.getElementById('message');
+    //     messageDiv.textContent = 'An error occurred. Please try again.';
+    //   }
+    // });
   }
 }
