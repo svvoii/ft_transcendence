5b06f505ee24f5b2a1631c69cafedcb1ff719a87
Drew <91377105+Drewski6@users.noreply.github.com>
2024-11-16 18:58:41 +0100

    Commented a couple things

--- _main_project/_main_project/urls.py
+++ _main_project/_main_project/urls.py
@@ -45,7 +45,7 @@ urlpatterns = [
 
 	# Password reset links (ref: https://github.com/django/django/blob/master/django/contrib/auth/views.py)
     path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(template_name='password_reset/password_change_done.html'), name='password_change_done'),
-    path('password_change/', auth_views.PasswordChangeView.as_view(template_name='password_reset/password_change.html'), name='password_change'),
+    # path('password_change/', auth_views.PasswordChangeView.as_view(template_name='password_reset/password_change.html'), name='password_change'),
     path('password_reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='password_reset/password_reset_done.html'), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'), 
     path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
