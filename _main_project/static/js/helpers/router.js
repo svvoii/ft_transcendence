4d80072483e52d20ed83c870c34afbdb04accac9
Drew <91377105+Drewski6@users.noreply.github.com>
2024-12-12 15:46:46 +0100

    Removed two files that I moved their content into other menu files. this pr merges several files into a smaller number of files in order to simplify the menu. Now GameOptions.js file content is in LocalMatchSelect.js and TournamentSetupJoin.js is in TournamentSelect.js

--- _main_project/static/js/helpers/router.js
+++ _main_project/static/js/helpers/router.js
@@ -1,12 +1,10 @@
 import Dashboard from "../views/Dashboard.js";
 import Settings from "../views/Settings.js";
 import GameMenu from "../views/GameMenu.js";
-import GameOptions from "../views/GameOptions.js";
 import TermsOfService from "../views/TermsOfService.js";
 import PrivacyPolicy from "../views/PrivacyPolicy.js";
 import AboutUs from "../views/AboutUs.js";
 import Page404 from "../views/Page404.js";
-import TournamentSetupJoin from "../views/TournamentSetupJoin.js";
 import TournamentSetupCreate from "../views/TournamentSetupCreate.js";
 import TournamentLobby from "../views/TournamentLobby.js";
 import TournamentSelect from "../views/TournamentSelect.js";
@@ -22,17 +20,12 @@ export const router = async () => {
     { path: '/', view: Dashboard },
     { path: '/settings/', view: Settings },
     { path: '/game_menu/', view: GameMenu },
-    { path: '/game_options/', view: GameOptions },
-    // { path: '/tournament_setup/', view: TournamentSetup },
     { path: '/tournament_lobby/', view: TournamentLobby },
     { path: '/tournament_select/', view: TournamentSelect },
     { path: '/multiplayer_select/', view: MultiplayerSelect },
     { path: '/local_match_select/', view: LocalMatchSelect },
     { path: '/tournament_setup_create/', view: TournamentSetupCreate },
-    { path: '/tournament_setup_join/', view: TournamentSetupJoin },
     { path: '/tournament_lobby/:tournament_name', view: TournamentLobby },
-    // { path: '/^\/tournament_lobby\/.*/', view: TournamentLobby },
-	  { path: '/game_options/', view: GameOptions },
     { path: '/terms_of_service/', view: TermsOfService },
     { path: '/privacy_policy/', view: PrivacyPolicy },
     { path: '/about_us/', view: AboutUs },
